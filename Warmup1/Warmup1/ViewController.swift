f2eaf9b768e132dad9cbc518138b7ce4414b2b00
Andrej Antas <andrej@antas.cz>
2016-01-06 13:56:34 +0100

    warmup01

---
+++ Warmup1/Warmup1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Warmup1
+//
+//  Created by Andrej Antas on 06/01/16.
+//  Copyright © 2016 Andrej Antas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
