4ea8799cb4e4dfb3b84a5995d682d630dbb49a52
Andrej Antas <andrej@antas.cz>
2016-01-06 11:42:41 +0100

    variable types playground :smile:

---
+++ variables-types.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground" // String
+
+str += "! I am so cool!"
+
+var numA = 1.5 //Double
+
+numA.description
+
+var numB: Float = 1.5 // Float
+
+var numC = 3 // Int
+
+var sum = 5 + 5
+var product = 4 * 5
+var divisor = 20 / 4
+var remainder = 22 % 5
+var sub = 44 - 22
+
+var crazy = sub - remainder + (product + divisor)
+
+var firstname = "Andrej"
+var lastname = "Antas"
+
+var name = "My name is \(firstname) \(lastname)"
+
+let prize: Float = 3.5
+
+var prizeString = "Prize is $\(prize)"
